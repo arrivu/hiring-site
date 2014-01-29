c0c3c4a86b6333add36967679a09e594e37ee392
franklinjagan <franklinjagan@arrivusystems.com>
2014-01-29 12:45:46 +0530

    header changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -490,7 +490,7 @@ class Course < ActiveRecord::Base
   scope :by_associated_accounts, lambda{ |account_ids|
     account_ids.empty? ?
       none :
-      where("EXISTS (?)", CourseAccountAssociation.where("course_account_associations.course_id=courses.id AND course_account_associations.account_id IN (?)", account_ids))
+      where("EXISTS (?)", CourseAccountAssociation.where("project_account_associations.course_id=courses.id AND project_account_associations.account_id IN (?)", account_ids))
   }
 
   scope :deleted, where(:workflow_state => 'deleted')
@@ -2410,7 +2410,7 @@ class Course < ActiveRecord::Base
   TAB_HOME = 0
   #TAB_SYLLABUS = 1
   TAB_PAGES = 1
-  #TAB_ASSIGNMENTS = 3
+  TAB_ASSIGNMENTS = 3
   TAB_QUIZZES = 2
   TAB_GRADES = 3
   TAB_PEOPLE = 4
@@ -2430,20 +2430,19 @@ class Course < ActiveRecord::Base
         { :id => TAB_HOME, :label => t('#tabs.home', "Home"), :css_class => 'home', :href => :course_path },
 
         #{ :id => TAB_ANNOUNCEMENTS, :label => t('#tabs.announcements', "Announcements"), :css_class => 'announcements', :href => :course_announcements_path },
-        #{ :id => TAB_ASSIGNMENTS, :label => t('#tabs.assignments', "Assignments"), :css_class => 'assignments', :href => :course_assignments_path },
+        { :id => TAB_ASSIGNMENTS, :label => t('#tabs.assignments', "Assignments"), :css_class => 'assignments', :href => :course_assignments_path },
         #{ :id => TAB_DISCUSSIONS, :label => t('#tabs.discussions', "Discussions"), :css_class => 'discussions', :href => :course_discussion_topics_path },
         { :id => TAB_GRADES, :label => t('#tabs.grades', "Results"), :css_class => 'grades', :href => :course_grades_path },
         { :id => TAB_PEOPLE, :label => t('#tabs.people', "People"), :css_class => 'people', :href => :course_users_path },
         { :id => TAB_CHAT, :label => t('#tabs.chat', "Chat"), :css_class => 'chat', :href => :course_chat_path },
-        { :id => TAB_PAGES, :label => t('#tabs.pages', "Pages"), :css_class => 'pages', :href => :course_wiki_pages_path },
+        #{ :id => TAB_PAGES, :label => t('#tabs.pages', "Pages"), :css_class => 'pages', :href => :course_wiki_pages_path },
         { :id => TAB_FILES, :label => t('#tabs.files', "Files"), :css_class => 'files', :href => :course_files_path },
         #{ :id => TAB_SYLLABUS, :label => t('#tabs.syllabus', "Syllabus"), :css_class => 'syllabus', :href => :syllabus_course_assignments_path },
-        { :id => TAB_OUTCOMES, :label => t('#tabs.outcomes', "Outcomes"), :css_class => 'outcomes', :href => :course_outcomes_path },
+        #{ :id => TAB_OUTCOMES, :label => t('#tabs.outcomes', "Outcomes"), :css_class => 'outcomes', :href => :course_outcomes_path },
         { :id => TAB_QUIZZES, :label => t('#tabs.quizzes', "Assessments"), :css_class => 'quizzes', :href => :course_quizzes_path },
         #{ :id => TAB_MODULES, :label => t('#tabs.modules', "Modules"), :css_class => 'modules', :href => :course_context_modules_path },
         { :id => TAB_CONFERENCES, :label => t('#tabs.conferences', "Conferences"), :css_class => 'conferences', :href => :course_conferences_path },
         { :id => TAB_COLLABORATIONS, :label => t('#tabs.collaborations', "Collaborations"), :css_class => 'collaborations', :href => :course_collaborations_path },
-
         { :id => TAB_SETTINGS, :label => t('#tabs.settings', "Settings"), :css_class => 'settings', :href => :course_settings_path },
     ]
   end
