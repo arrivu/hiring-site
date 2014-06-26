ed23fe4e4d26ec214c019fd9fd9d03b0206af593
abdul18 <abdul@arrivusystems.com>
2014-06-26 16:02:10 +0530

    sis import tag add and tag filter bank select tag questions count display work finished

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -41,23 +41,42 @@ class TagsController < ApplicationController
       end
     end
   end
+  #def get_tag_to_bank
+  #  if params[:tag_id]
+  #    @tag_bank = []
+  #    @tag_id = []
+  #    @tag_id = params[:tag_id]
+  #    @bank_id = ActsAsTaggableOn::Tagging.find_all_by_tag_id(params[:tag_id])
+  #    @bank_id.each do |tagger|
+  #      @tagger_id = tagger.tagger_id
+  #      @taggable_id = tagger.taggable_id
+  #      @bank = AssessmentQuestionBank.find_by_id(@tagger_id)
+  #      @bank.tag_id = params[:tag_id]
+  #      @tag_bank << @bank
+  #      @tag_bank_filter = @tag_bank.uniq
+  #    end
+  #    respond_to do |format|
+  #      format.json { render :json =>   @tag_bank_filter.map{ |b| b.as_json(methods: [:cached_context_short_name, :assessment_question_count]) }}
+  #    end
+  #  end
+  #end
   def get_tag_to_bank
-    if params[:tag_id]
+    if params[:tag_id] && params[:bank_id]
       @tag_bank = []
       @tag_id = []
-      @tag_id = params[:tag_id]
-      @bank_id = ActsAsTaggableOn::Tagging.find_all_by_tag_id(params[:tag_id])
-      @bank_id.each do |tagger|
-        @tagger_id = tagger.tagger_id
-        @taggable_id = tagger.taggable_id
-        @bank = AssessmentQuestionBank.find_by_id(@tagger_id)
+      #@tag_id = params[:tag_id]
+      #@bank_id = ActsAsTaggableOn::Tagging.find_all_by_tag_id(params[:tag_id])
+      #@bank_id.each do |tagger|
+      #  @tagger_id = tagger.tagger_id
+      #  @taggable_id = tagger.taggable_id
+        @bank = AssessmentQuestionBank.find_by_id(params[:bank_id])
         @bank.tag_id = params[:tag_id]
         @tag_bank << @bank
         @tag_bank_filter = @tag_bank.uniq
       end
       respond_to do |format|
         format.json { render :json =>   @tag_bank_filter.map{ |b| b.as_json(methods: [:cached_context_short_name, :assessment_question_count]) }}
-      end
+      #end
     end
   end
 end
